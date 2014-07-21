a1e30bfbf6090fe3b4382e47c7ee8ac33a150adb
= <=>
2014-07-21 21:10:14 +0800

    添加了验证传数的函数
    参数形式为[ [ [ a],[b],[c]]  , [d,f] ] 或
    { data:[ [a] , [b] ,[c] ], css : [d,f] }

--- display.js
+++ display.js
@@ -1,39 +1,101 @@
-(function(_global){
+( function(_global) {
+		//model collection object
+		var liContainer;
 	
-	var SingleItem = Backbone.Model.extend({
-		
-		initialize:function(){
+		var backbone = function() {
 			
-		},
-		
-		defaults:{
-			top:0,
-			left:0,
-			opacity:1
+			var SingleItem = Backbone.Model.extend({
+
+				url:"#",
+				
+				initialize : function(id) {
+					
+					this.bind("invalid",function(model,error){
+
+					});
+				},
+				validate:function(attr){
+
+				},
+				defaults : {
+					name:"d"
+				}
+			});
+
+			var Items = Backbone.Collection.extend({
+				model : "SingleItem",
+			});
+			
+			var itemsObj = new Items();
+
+			return {
+				"SingleItem":SingleItem,
+				"Items":itemObj
+			};
+		}();
+
+		function init(_t,_arg){
+			
+			var fn = {"load":load,"show":show};
+
+			if(typeof _t == "string"){
+				return fn[_t]?fn[_t](_arg):fn[0](_arg);
+			}else{
+				throw new Error("invalid type of arguments in function named init");
+			}
 		}
-	});
-	
-	var Items = Backbone.Collection.extend({
-		model:"SingleItem"
-	});
-	
-	var ContainerView = Backbone.View.extend({
-		
-		el:null,
+		var parse = function(_arg){
+			// _arg = [ [ [1,2,3],[1,2,3],[1,2,3] ]   , [css1,css2,css3]]
+			//3维数组
+			if(_arg.constructor == Array){
 
-		initialize:function(context){
-
-		},
-		
-		events:{
-			"click":"toggle"
-		},
-		
-		toggle:function(){
-
+				if(_arg.length != 2){
+					throw new Error("the argument's length is illegal");
+					return;
+				}
+			}else if(_arg.constructor == Object){
+				
+				if(_arg.data){
+					throw new Error("the argument Object have to include data");
+					return;
+				}
+				if(_arg.css){
+					throw new Error("the argument Object have to include css");
+					return;
+				}
+				_arg = [_arg.data,_arg.css];
+			}
+			//第一个数组元素是数据
+			if(_arg[0].length && _arg[0][0].length && _arg[1].length){
+				return ;
+			}
+			
+			var l = _arg[0][0].length;
+			var r = _arg[0].every(function(el){
+				return el.length==l?true:false;
+			});
+			if(!r){
+				throw new Error("all the data arrays' lengthes are not equal");
+				return;
+			}
+			
+			r = _arg[1].every(function(el){
+				return el;
+			});
+			if(!r){
+				throw new Error("all the css arrays' data is illegal");
+				return;
+			}
+			return _arg;
+		};
+		function load(_arg){
+			
+			var dataArr = parse(_arg);
 		}
-	});
-	
-	var cv = new ContainerView({el:$("#container")});
-	
-}(window));
+		function show(_arg){
+			
+		}
+		
+		
+		_global.init = init;
+	}(window)); 
